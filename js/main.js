29422e71ebbeae737880566145d12f87ba103d5e
Kauã Francino <163582383+Nosferatzod@users.noreply.github.com>
2025-05-06 10:29:09 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -574,7 +574,7 @@ function saveScore() {
 function getRanking() {
   // Ranking inicial pré-definido
   const initialRanking = [
-    { name: "Cesar", score: 5118 },
+    { name: "Cesar", score: 6716 },
     { name: "Ygor", score: 3740 },
     { name: "Kauã Francino", score: 3630 },
     { name: "Kauã Araújo", score: 1582 },
